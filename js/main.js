07d8accf8ee5d61a248bf5a8dfdc7c2ef9d7d631
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-02-06 12:52:59 -0300

    Se rellena json

--- js/main.js
+++ js/main.js
@@ -13,3 +13,48 @@ const initApp = () => {
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
+
+
+
+
+
+`<div class="card card-body mt-3">
+<div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
+    <div class="mr-2 mb-3 mb-lg-0">
+        
+            <img src="/img/quesocabra.jpg.jpeg" width="20vw" alt="">
+       
+    </div>
+
+    <div class="media-body">
+        <h6 class="media-title font-weight-semibold">
+            <a href="#" data-abc="true">Queso de cabra "La pradera"</a>
+        </h6>
+
+        <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
+            <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Queso</a></li>
+            <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Extracción animal</a></li>
+        </ul>
+
+        <p class="mb-3">Vendo exquisito queso de cabra madurado, realizado por artesanos de Coyaritahue </p>
+
+
+    </div>
+
+    <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
+        <h3 class="mb-0 font-weight-semibold">$5000 x Kilo</h3>
+
+        <div>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+
+        </div>
+
+        <div class="text-muted">A 100 Metros de ti</div>
+
+    </div>
+</div>
+</div> `
