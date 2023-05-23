45c56b5227ee890cf7921eb04de6c288d06f104f
andrescarlos-gm <andrescarlos2211@gmail.com>
2023-05-23 11:45:25 -0400

    agrega confirmacion loggeo

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ app.post('/catalogo', async function (req, res) {
     let busqueda = req.body.busqueda;
     const response = await fetch(`https://api-qcc.onrender.com/api/v1/simplesearch/${busqueda}`)
     const data = await response.json(); // Convertir la respuesta en formato JSON
-    res.render('catalogo', { data });
+    res.render('catalogo', { data, isLoggedIn: req.user });
 })
 app.get('/nosotros', function (req, res) {
     res.render('nosotros')
