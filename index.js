31f796da78a839991a79c2a7a29f1e21b5cc3d69
andrescarlos-gm <andrescarlos2211@gmail.com>
2023-05-23 21:23:02 -0400

    test error busqueda vacia

--- index.js
+++ index.js
@@ -139,7 +139,16 @@ app.post('/catalogo', async function (req, res) {
     let busqueda = req.body.busqueda;
     const response = await fetch(`https://api-qcc.onrender.com/api/v1/simplesearch/${busqueda}`)
     const data = await response.json(); // Convertir la respuesta en formato JSON
-    res.render('catalogo', { data, isLoggedIn: req.user });
+    
+    if (Object.keys(data).length === 0) {
+        // JSON vacío
+        console.log('El JSON está vacío');
+        res.render('catalogo', { data: null, isLoggedIn: req.user });
+    } else {
+        // JSON con datos
+        console.log(data);
+        res.render('catalogo', { data, isLoggedIn: req.user });
+    }
 })
 app.get('/nosotros', function (req, res) {
     res.render('nosotros')
